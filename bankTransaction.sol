71b1c2a192c8322dd43b81fcc685b20f6412a052
Lennox Matwere <aberamacarleton@gmail.com>
2024-07-06 23:09:49 +0300

    Refactor ETH-bank:
    Remove transactions array,Use Dynamic Mappings, update getClientInfo, and optimize gas usage

--- bankTransaction.sol
+++ bankTransaction.sol
@@ -3,44 +3,28 @@
 pragma solidity >0.8.1;
 
 /* 
-Two possible approaches:
-    1.) create transactionss[] array with elements being of the transaction object
-    2.) map transactions to a unique id for each transacton
-        The transaction identifier can either be:
-            i.)  Combination of deterministic letters and numbers, requires an extra array for case 2 above to track the ID's
-            ii.) Incrementing an integer gradualy; "cheap" for option 2
-Commented out option 1 compiled option 2
+map transactions to a unique id for each transaction
  */
-contract BankTransaction{
-    struct Transaction{
-        uint ID; //payment identifier from 0...n0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+ 
+contract BankTransaction {
+    struct Transaction {
+        uint ID; // payment identifier from 0...n0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
         address client;
         address payable recipient;
         uint amount;
         uint timestamp;
         string note;
-        bytes32 hashed;// hash value of payment
+        bytes32 hashed; // hash value of payment
     }
 
-    // Transaction[] public transactions;
-    mapping (uint=>Transaction) txs;
+    // Transaction[] public transactions; // Removed array logic
+    mapping (uint => Transaction) txs; // Maintain a mapping of transaction IDs to Transaction objects
     uint public count = 0;
 
-//Adding a new payment
+    // Adding a new payment
     function addTransaction(address payable receiver, uint amount, string memory note) public {
         uint time = block.timestamp;
         uint id = generateID();
-        /* transactions.push(
-            Transaction({
-                ID: id,
-                client: msg.sender,
-                recipient: receiver,
-                amount: amount,
-                timestamp: time,
-                note: note,
-                hashed: ""
-            })
-        ); */
         txs[id] = Transaction(
             {
                 ID: id,
@@ -49,47 +33,52 @@ contract BankTransaction{
                 amount: amount,
                 timestamp: time,
                 note: note,
-                hashed: keccak256(bytes.concat(abi.encodePacked(time),abi.encodePacked(msg.sender),abi.encodePacked(receiver),abi.encodePacked(amount)))
+                hashed: keccak256(bytes.concat(
+                    abi.encodePacked(time),
+                    abi.encodePacked(msg.sender),
+                    abi.encodePacked(receiver),
+                    abi.encodePacked(amount)
+                ))
             }
         );
     }
-//Generate transaction ID 
-    function generateID() public returns (uint){
-        count+=1;
+
+    // Generate transaction ID 
+    function generateID() public returns (uint) {
+        count += 1;
         return count;
     }
 
-//Getting information about the payment by it's identifier
-    function getTransaction(uint id) public view returns(Transaction memory){
-        /* for (uint256 i = 0; i < transactions.length; i++) {
-            if(keccak256(abi.encodePacked(transactions[i].ID))==keccak256(abi.encodePacked(id))){
-                return transactions[i];
-            }
-        } */
+    // Getting information about the payment by its identifier
+    function getTransaction(uint id) public view returns (Transaction memory) {
         return txs[id];
     }
-//Getting all payments of a particular customer
-    function getClientInfo(address client) public view returns(Transaction[] memory clientTxs){
-        /* for (uint256 i = 0; i < transactions.length; i++) {
-            if(transactions[i].client==client){
-                return transactions[i];
+
+    // Getting all payments of a particular customer
+    function getClientInfo(address client) public view returns (Transaction[] memory clientTxs) {
+        uint txCount = 0;
+        for (uint256 i = 1; i <= count; i++) {
+            if (txs[i].client == client) {
+                txCount++;
             }
-        } */
-        uint8 x = 0; //variable to point to index to store transaction in array
-        for (uint256 i = 0; i <= count; i++) {
-            if(txs[i].client==client){
-                require(x!=5, "Can only pull the first 5 transactions");
-                clientTxs[x]=(txs[i]);
-                x+=1;
-                // clientTxs = txs[i];
+        }
+
+        clientTxs = new Transaction[](txCount);
+        uint8 x = 0; // variable to point to index to store transactions in the array
+        for (uint256 i = 1; i <= count; i++) {
+            if (txs[i].client == client) {
+                require(x < 5, "Can only pull the first 5 transactions");
+                clientTxs[x] = txs[i];
+                x += 1;
             }
         }
     }
-//Additional task
-    function additionalTask() public pure returns(uint answer){
+
+    // Gas Optimization
+    function gasOptimization() public pure returns (uint answer) {
         uint a = 3;
         uint b = 2;
         uint accuracy = 10**8;
-        answer = (a*accuracy)/(b); //on fronten divide answer by accuracy
+        answer = (a * accuracy) / (b); // on frontend divide answer by accuracy
     }
 }
